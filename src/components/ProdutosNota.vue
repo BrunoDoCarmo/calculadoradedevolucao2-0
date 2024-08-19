7c746dacbd666c3a224108803619b9095674fe5d
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2024-08-19 17:56:39 -0300

    Alteração na importação do xml e criação de abas dentro do componente produtoNota

--- src/components/ProdutosNota.vue
+++ src/components/ProdutosNota.vue
@@ -6,39 +6,68 @@
     <!-- Mostra os produtos da nota fiscal ativa -->
     <div v-if="notas.length">
       <div v-for="(nota, index) in notas" :key="index" v-show="activeTab === index">
-        <div class="table-container">
-          <table class="produtos-tabela">
-            <thead>
-              <tr>
-                <th>Cod. Prod.</th>
-                <th>Nome Produto</th>
-                <th>QTD</th>
-                <th>VLR Unit.</th>
-                <th>VLR Total</th>
-                <th>Ação</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr 
-                v-for="(produto, prodIndex) in nota.produtos" 
-                :key="prodIndex" 
-                :class="{ 'linha-marcada': produto.selecionado }"
-              >
-                <td>{{ produto.cProd }}</td>
-                <td>{{ produto.xProd }}</td>
-                <td>{{ produto.qCom }}</td>
-                <td>{{ produto.vUnCom }}</td>
-                <td>{{ calcularValorTotal(produto) }}</td>
-                <td>
-                  <input 
-                    type="checkbox" 
-                    v-model="produto.selecionado"
-                    @change="emitSelectedProducts"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <Abas :abas="subtabs" v-model:activeTab="activeSubTab" />
+        <div v-show="activeSubTab === 0">
+          <div class="table-container">
+            <table class="produtos-tabela">
+              <thead>
+                <tr>
+                  <th>Cod. Prod.</th>
+                  <th>Nome Produto</th>
+                  <th>QTD</th>
+                  <th>VLR Unit.</th>
+                  <th>VLR Desc.</th>
+                  <th>VLR Total</th>
+                  <th>Ação</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr 
+                  v-for="(produto, prodIndex) in nota.produtos" 
+                  :key="prodIndex" 
+                  :class="{ 'linha-marcada': produto.selecionado }"
+                >
+                  <td>{{ produto.cProd }}</td>
+                  <td>{{ produto.xProd }}</td>
+                  <td>{{ produto.qCom }}</td>
+                  <td>{{ produto.vUnCom }}</td>
+                  <td>{{ produto.vDesc }}</td>
+                  <td>{{ calcularValorTotal(produto) }}</td>
+                  <td>
+                    <input 
+                      type="checkbox" 
+                      v-model="produto.selecionado"
+                      @change="emitSelectedProducts"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div v-show="activeSubTab === 1">
+          <div class="table-container">
+            <table class="imposto-tabela">
+              <thead>
+                <tr>
+                  <th>BC ICMS</th>
+                  <th>Alíquota ICMS</th>
+                  <th>Valor ICMS</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr 
+                  v-for="(produto, prodIndex) in nota.produtos" 
+                  :key="prodIndex" 
+                  :class="{ 'linha-marcada': produto.selecionado }"
+                >
+                  <td>{{ calcularDesconto(produto) }}</td>
+                  <td>{{ produto.pICMS }}</td>
+                  <td>{{ produto.qCom }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -63,6 +92,11 @@ export default {
   data() {
     return {
       activeTab: 0, // Inicia com a primeira aba ativa
+      activeSubTab: 0, // Inicia com a primeira aba ativa
+      subtabs: [
+          'Produtos',
+          'Imposto'
+      ],
     };
   },
   computed: {
@@ -78,7 +112,18 @@ export default {
       const valorUnitario = parseFloat(produto.vUnCom.replace(/[^\d,.-]/g, '').replace(",", "."));
       const quantidade = parseFloat(produto.qCom);
       if (!isNaN(quantidade) && !isNaN(valorUnitario) && quantidade >= 0) {
-        return (quantidade * valorUnitario).toFixed(2);
+        return (quantidade * valorUnitario).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+      } else {
+        return "0.00";
+      }
+    },
+    calcularDesconto(produto) {
+      const valorTotal = this.calcularValorTotal(produto);
+      const valorDesconto = parseFloat(produto.vDesc.replace(/[^\d,.-]/g, '').replace(",", "."));
+      const valorComDesconto = valorTotal - valorDesconto;
+
+      if (!isNaN(valorComDesconto) && valorComDesconto >= 0) {
+        return valorComDesconto.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
       } else {
         return "0.00";
       }
@@ -132,15 +177,19 @@ export default {
   width: 12%;
 }
 .table-container th:nth-child(3),
-.table-container td:nth-child(3),
-.table-container th:nth-child(6),
-.table-container td:nth-child(6) {
+.table-container td:nth-child(3) {
   width: 8%;
+}
+.table-container th:nth-child(7),
+.table-container td:nth-child(7) {
+  width: 5%;
 }
 .table-container th:nth-child(4),
 .table-container td:nth-child(4),
 .table-container th:nth-child(5),
-.table-container td:nth-child(5) {
+.table-container td:nth-child(5),
+.table-container th:nth-child(6),
+.table-container td:nth-child(6) {
   width: 10%;
 }
 .table-container td:nth-child(2) {
